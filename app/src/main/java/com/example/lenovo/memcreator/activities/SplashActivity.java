b10e31ddef92705b549a39a9d0193a30262ec9d8
ahqmrf <ahqmrf@gmail.com>
2017-01-30 19:17:31 +0600

    added progressbar

---
+++ app/src/main/java/com/example/lenovo/memcreator/activities/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.lenovo.memcreator.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.lenovo.memcreator.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
